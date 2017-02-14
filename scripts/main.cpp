f7f4b5b371b91cc19124fdc99d66d0fa23a40b94
GrumpyCoder <salleharifin100@outlook.sg>
2017-02-14 20:43:31 +0800

    Update main.cpp

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -3,5 +3,6 @@ wall → set background(colors → random → darken(0.4))
 wall → show back button(false)
 data → board := media → create landscape board(1920, 1080)
 data → board → set background(colors → black)
+code → displayReadyText
 data → board → post to wall
 }
