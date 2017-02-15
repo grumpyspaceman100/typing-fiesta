12b72c02f72b2425de365183812c85c49372d394
GrumpyCoder <salleharifin100@outlook.sg>
2017-02-15 20:27:00 +0800

    Create createLetters.hpp

---
+++ scripts/createLetters.hpp
@@ -0,0 +1,9 @@
+private function
+createLetters
+() {
+code → createletter_a(false)
+code → createletter_b(false)
+code → createletter_c(false)
+code → createletter_d(false)
+code → createletter_e(false)
+}
