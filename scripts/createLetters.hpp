f2904ec179837b8bdd8c38209f48486aaf9d6a75
grumpyspaceman100 <grumpy_spaceman100@outlook.com>
2017-02-17 18:40:26 +0800

    Update createLetters.hpp

--- scripts/createLetters.hpp
+++ scripts/createLetters.hpp
@@ -1,9 +1,20 @@
 private function
 createLetters
-() {
+(
+globalshow : Boolean)
+{
+if (globalshow → equals(true)) {
+code → createletter_a(true)
+code → createletter_b(true)
+code → createletter_c(true)
+code → createletter_d(true)
+code → createletter_e(true)
+code → createletter_f(true)
+} else { }
 code → createletter_a(false)
 code → createletter_b(false)
 code → createletter_c(false)
 code → createletter_d(false)
 code → createletter_e(false)
+code → createletter_f(false)
 }
